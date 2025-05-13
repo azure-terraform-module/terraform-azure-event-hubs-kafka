980edc6819e64a16d23e86c05580eb28013a4bb4
baominh <haphanbaominh.dev@gmail.com>
2025-05-13 09:52:38 +0700

    update output after review

--- c6-outputs.tf
+++ c6-outputs.tf
@@ -1,8 +1,3 @@
-output "namespace_name" {
-  description = "The name of the Event Hub Namespace"
-  value       = azurerm_eventhub_namespace.eventhub_namespace.name
-}
- 
 output "namespace" {
   description = "The name of the Event Hub Namespace"
   value       = azurerm_eventhub_namespace.eventhub_namespace.name
@@ -15,7 +10,7 @@ output "namespace_id" {
 
 output "topics" {
   description = "The list of topics in the Event Hub Namespace"
-  value       = azurerm_eventhub_namespace.eventhub_namespace.eventhub_names
+  value       = [for eh in azurerm_eventhub.eventhub : eh.name]
 }
  
 output "hostname" {
